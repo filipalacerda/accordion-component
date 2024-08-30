5afc846bc5da22753d1637ecc9e345c4876ec1c2
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-08-30 12:39:29 +0100

    Adds unit tests

--- src/components/accordion/index.test.tsx
+++ src/components/accordion/index.test.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import Accordion from ".";
+import userEvent from "@testing-library/user-event";
 
 describe("Accordion", () => {
+  const title = "Title 1";
+  const content = "This is the content";
+
   describe("when component renders", () => {
     let container: HTMLElement;
 
     beforeEach(() => {
       container = render(
-        <Accordion title="Title 1" content="This is the content" />
+        <Accordion title={title} content={content} />
       ).container;
     });
 
@@ -17,7 +21,7 @@ describe("Accordion", () => {
     });
 
     it("should render the provided title", () => {
-      expect(screen.getByText("Title 1")).toBeInTheDocument();
+      expect(screen.getByText(title)).toBeInTheDocument();
     });
 
     it("should render the open accordion icon", () => {
@@ -33,20 +37,44 @@ describe("Accordion", () => {
     });
   });
 
-  describe("with disabled property", () => {});
+  describe("with disabled property", () => {
+    it("should not open the accordion when user clicks", async () => {
+      render(<Accordion title={title} content={content} isDisabled />);
 
-  describe("with isOpenDefault", () => {});
+      expect(screen.queryByTestId("arrow-up")).not.toBeInTheDocument();
+      await userEvent.click(screen.getByTestId("accordion-button"));
+
+      expect(screen.queryByTestId("arrow-up")).not.toBeInTheDocument();
+      expect(screen.queryByTestId("accordion-content")).not.toBeInTheDocument();
+      expect(screen.getByTestId("arrow-down")).toBeInTheDocument();
+    });
+  });
+
+  describe("with defaultExpanded", () => {
+    it("should render the accordion opened", () => {
+      render(<Accordion title={title} content={content} defaultExpanded />);
+
+      // Open accordion icon
+      expect(screen.queryByTestId("arrow-down")).not.toBeInTheDocument();
+      // Close accordion icon
+      expect(screen.getByTestId("arrow-up")).toBeInTheDocument();
+
+      expect(screen.getByTestId("accordion-content")).toBeInTheDocument();
+    });
+  });
 
   describe("keydown events", () => {
     let container: HTMLElement;
     let button: HTMLElement;
+
     beforeEach(() => {
       container = render(
-        <Accordion title="Title 1" content="This is the content" />
+        <Accordion title={title} content={content} />
       ).container;
 
       button = screen.getByTestId("accordion-button");
     });
+
     describe("when the user presses enter key", () => {
       it("should open the accordion", async () => {
         await fireEvent.keyDown(button, {
@@ -58,6 +86,7 @@ describe("Accordion", () => {
         expect(screen.getByTestId("accordion-content")).toBeInTheDocument();
       });
     });
+
     describe("when the user presses arrowDown key", () => {
       it("should open the accordion", async () => {
         await fireEvent.keyDown(button, {
